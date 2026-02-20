6f4e38d13dee8e14daabdc4404b8578454b8b3bc
Duncan Rose <DuncanRose@users.noreply.github.com>
2026-02-20 16:44:16 -0500

    Update phloem to v0.1.3

--- Formula/phloem.rb
+++ Formula/phloem.rb
@@ -6,15 +6,15 @@
 class Phloem < Formula
   desc "Local-first AI memory with causal graphs via MCP"
   homepage "https://github.com/CanopyHQ/phloem"
-  version "0.1.2"
+  version "0.1.3"
   license "Apache-2.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/CanopyHQ/phloem/releases/download/v0.1.2/phloem-darwin-arm64.tar.gz"
-    sha256 "07b78eb72eaaf12d550c5c7942b093c33f7a348358740541c75785b9d79e3294"
+    url "https://github.com/CanopyHQ/phloem/releases/download/v0.1.3/phloem-darwin-arm64.tar.gz"
+    sha256 "ab86870f2121e5ab1e247f9b0fc1dd25d705f8855439848b55686802bbbfd256"
   else
-    url "https://github.com/CanopyHQ/phloem/releases/download/v0.1.2/phloem-darwin-amd64.tar.gz"
-    sha256 "48bff92d3e78324ce53d0e73243afbc6472b26b67a534a8a1a140a09d0eee277"
+    url "https://github.com/CanopyHQ/phloem/releases/download/v0.1.3/phloem-darwin-amd64.tar.gz"
+    sha256 "d2d1cdbfce8a679cd0f8669a8681f5dbfcab6bd4929acb35ae5bf072dc3d8a19"
   end
 
   def install
